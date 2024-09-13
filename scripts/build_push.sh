214fd5b033b9317d192518fde72140dcf0a3314f
fox-R-fox <rohansherkar2207@gmail.com>
2024-09-13 10:10:37 +0530

    uploaded files

---
+++ scripts/build_push.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+REPO_URL ="public.ecr.aws/k0n5u3j8"
+IMAGE_TAG="latest"
+AWS_REGION="ap-south-1"
+SECRET_ARN='arn:aws:secretsmanager:ap-south-1:339712721384:secret:dockerhublogin-4vcmMW'
+
+DOCKER_USERNAME=$(aws secretmanager get-secret-value --secret-id &SECRET_ARN --query 'SecretString' --output text | jq -r .username)
+DOCKER_PASSWORD=$(aws secretmanager get-secret-value --secret-id &SECRET_ARN --query 'SecretString' --output text | jq -r .password)
+
+aws ecr-public get-login-password --region $AWS_REGION | docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD $REPO_URL
+
+docker build -t $REPO_URL:$IMAGE_TAG
+
+docker push $REPO_URL:$IMAGE_TAG
