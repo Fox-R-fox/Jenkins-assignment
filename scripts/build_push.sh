e17b927d62c208528bfbeff122d511051d1c484d
Fox-R-fox <rohansherkar2207@gmail.com>
2024-09-13 10:34:52 +0530

    Update build_push.sh

--- scripts/build_push.sh
+++ scripts/build_push.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-REPO_URL ="public.ecr.aws/k0n5u3j8"
+# Set variables
+REPO_URL="public.ecr.aws/k0n5u3j8/test"
 IMAGE_TAG="latest"
 AWS_REGION="ap-south-1"
-SECRET_ARN='arn:aws:secretsmanager:ap-south-1:339712721384:secret:dockerhublogin-4vcmMW'
+SECRET_ARN="arn:aws:secretsmanager:ap-south-1:339712721384:secret:dockerhublogin-4vcmMW"
 
-DOCKER_USERNAME=$(aws secretmanager get-secret-value --secret-id &SECRET_ARN --query 'SecretString' --output text | jq -r .username)
-DOCKER_PASSWORD=$(aws secretmanager get-secret-value --secret-id &SECRET_ARN --query 'SecretString' --output text | jq -r .password)
+# Get Docker credentials from AWS Secrets Manager
+DOCKER_USERNAME=$(aws secretsmanager get-secret-value --secret-id $SECRET_ARN --query 'SecretString' --output text | jq -r .username)
+DOCKER_PASSWORD=$(aws secretsmanager get-secret-value --secret-id $SECRET_ARN --query 'SecretString' --output text | jq -r .password)
 
-aws ecr-public get-login-password --region $AWS_REGION | docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD $REPO_URL
+# Login to AWS ECR
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $REPO_URL
 
-docker build -t $REPO_URL:$IMAGE_TAG
+# Build the Docker image
+docker build -t $REPO_URL:$IMAGE_TAG .
 
+# Push the image to ECR
 docker push $REPO_URL:$IMAGE_TAG
