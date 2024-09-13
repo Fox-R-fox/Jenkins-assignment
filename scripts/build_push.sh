fd502f042fabd91a68db512fd1cbde9598dc369e
fox-R-fox <rohansherkar2207@gmail.com>
2024-09-13 16:20:24 +0530

    updated

--- scripts/build_push.sh
+++ scripts/build_push.sh
@@ -6,10 +6,10 @@ IMAGE_NAME="your_dockerhub_username/app_name"
 TAG="latest"
 
 # Log in to Docker Hub
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-# Build the Docker image
-docker build -t ${IMAGE_NAME}:${TAG} .
+# Build the Docker image, specifying the path to the Dockerfile
+docker build -t ${IMAGE_NAME}:${TAG} -f ./app/Dockerfile .
 
 # Push the Docker image to Docker Hub
 docker push ${IMAGE_NAME}:${TAG}
